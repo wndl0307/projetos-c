e6c1f8bc2e5a701b043c1f82aec3dd36d463cdbe
Josué Wendel <wendeljosue9@gmail.com>
2026-04-21 22:37:22 -0300

    Update reajuste-salarial.c

--- projetos/reajuste-salarial.c
+++ projetos/reajuste-salarial.c
@@ -10,26 +10,26 @@ int main () {
         printf ("-----------------------------------------------\n");
         printf ("---------------REAJUSTE SALARIAL---------------\n");
         printf ("-----------------------------------------------\n");
-        printf ("Informe o nome do funcionário ou pressione S/s para encerrar o programa: ");
-        scanf (" %s", &nome_f);
-        printf ("Informe o documento de identificação: \n");
+        printf ("Informe o nome do funcionario ou pressione S/s para encerrar o programa: \n");
+        scanf (" %s", nome_f);
+        printf ("Informe o documento de identificacao: \n");
         scanf ("%i", &doc_i);
-        printf ("Informe o tempo de serviço: \n");
+        printf ("Informe os anos de servico: \n");
         scanf ("%i", &temp_s);
-        printf ("Informe o salário atual: \n");
+        printf ("Informe o salario atual: \n");
         scanf ("%f", &salario_a);
         printf ("\n");
 
         if (salario_a < 1500.) {
-            printf ("Faixa salarial: %.2f\n", salario_a);
+            printf ("Faixa salarial: R$%.2f\n", salario_a);
             calculo_percentual_1 = salario_a * 0.15;
-            printf ("Percentual de reajuste correspondente ao salário atual: 15%% \n");
-            printf ("Tempo de serviço: %i\n", temp_s);
+            printf ("Percentual de reajuste correspondente ao salario atual: 15%% \n");
+            printf ("Tempo de servico: %i anos\n", temp_s);
             printf ("\n");
 
             if (temp_s > 5) {
                 calculo_percentual_2 = salario_a * 0.02;
-                printf ("Bônus adicional devido ao tempo de serviço: 2%% \n");
+                printf ("Bonus adicional devido ao tempo de servico: 2%% \n");
                 printf ("\n");
             }
             salario_total = salario_a + calculo_percentual_1 + calculo_percentual_2;
@@ -45,31 +45,31 @@ int main () {
 
             if (salario_total < 1621.) {
                 printf ("***************ALERTA***************\n");
-                printf ("*Salário inferior ao salário mínimo!*\n");
+                printf ("*Salario inferior ao salario minimo!*\n");
                 printf ("***************ALERTA***************\n");
                 printf ("\n");
 
 
             }
-            printf ("Salário anterior: %.2f\n", salario_a);
+            printf ("Salario anterior: R$%.2f\n", salario_a);
             printf ("Percentual aplicado: 15%%\n");
-            printf ("Valor do aumento: %.2f\n", valor_aumento);
-            printf ("Novo salário: %.2f\n", salario_total);
+            printf ("Valor do aumento: R$%.2f\n", valor_aumento);
+            printf ("Novo salario: R$%.2f\n", salario_total);
             printf ("\n");
 
         }
 
         else if (salario_a >= 1500. && salario_a < 3000.) {
-            printf ("Faixa salarial: %.2f\n", salario_a);
+            printf ("Faixa salarial: R$%.2f\n", salario_a);
             calculo_percentual_1 = salario_a * 0.10;
-            printf ("Percentual de reajuste correspondente ao salário atual: 10%% \n");
-            printf ("Tempo de serviço: %i\n", temp_s);
+            printf ("Percentual de reajuste correspondente ao salario atual: 10%% \n");
+            printf ("Tempo de servico: %i anos\n", temp_s);
             printf ("\n");
 
 
             if (temp_s > 5) {
                 calculo_percentual_2 = salario_a * 0.02;
-                printf ("Bônus adicional devido ao tempo de serviço: 2%%\n");
+                printf ("Bonus adicional devido ao tempo de servico: 2%%\n");
                 printf ("\n");
 
             }
@@ -86,30 +86,30 @@ int main () {
 
             if (salario_total < 1621.) {
                 printf ("***************ALERTA***************\n");
-                printf ("*Salário inferior ao salário mínimo!*\n");
+                printf ("*Salario inferior ao salario minimo!*\n");
                 printf ("***************ALERTA***************\n");
                 printf ("\n");
 
             }
-            printf ("Salário anterior: %.2f\n", salario_a);
+            printf ("Salario anterior: R$%.2f\n", salario_a);
             printf ("Percentual aplicado: 10%%\n");
-            printf ("Valor do aumento: %.2f\n", valor_aumento);
-            printf ("Novo salário: %.2f\n", salario_total);
+            printf ("Valor do aumento: R$%.2f\n", valor_aumento);
+            printf ("Novo salario: R$%.2f\n", salario_total);
             printf ("\n");
 
             }
 
         else if (salario_a >= 3000. && salario_a < 5000.) {
-            printf ("Faixa salarial: %.2f\n", salario_a);
+            printf ("Faixa salarial: R$%.2f\n", salario_a);
             calculo_percentual_1 = salario_a * 0.07;
-            printf ("Percentual de reajuste correspondente ao salário atual: 7%%\n");
-            printf ("Tempo de serviço: %i\n", temp_s);
+            printf ("Percentual de reajuste correspondente ao salario atual: 7%%\n");
+            printf ("Tempo de servico: %i anos\n", temp_s);
             printf ("\n");
 
 
             if (temp_s > 5) {
                 calculo_percentual_2 = salario_a * 0.02;
-                printf ("Bônus adicional devido ao tempo de serviço: 2%%\n");
+                printf ("Bonus adicional devido ao tempo de servico: 2%%\n");
                 printf ("\n");
 
             }
@@ -126,29 +126,29 @@ int main () {
 
             if (salario_total < 1621.) {
                 printf ("***************ALERTA***************\n");
-                printf ("*Salário inferior ao salário mínimo!*\n");
+                printf ("*Salario inferior ao salario minimo!*\n");
                 printf ("***************ALERTA***************\n");
                 printf ("\n");
 
             }
-            printf ("Salário anterior: %.2f\n", salario_a);
+            printf ("Salario anterior: R$%.2f\n", salario_a);
             printf ("Percentual aplicado: 7%%\n");
-            printf ("Valor do aumento: %.2f\n", valor_aumento);
-            printf ("Novo salário: %.2f\n", salario_total);
+            printf ("Valor do aumento: R$%.2f\n", valor_aumento);
+            printf ("Novo salario: R$%.2f\n", salario_total);
             printf ("\n");
 
         }
         else {
-            printf ("Faixa salarial: %.2f\n", salario_a);
+            printf ("Faixa salarial: R$%.2f\n", salario_a);
             calculo_percentual_1 = salario_a * 0.05;
-            printf ("Percentual de reajuste correspondente ao salário atual: 5%%\n");
-            printf ("Tempo de serviço: %i\n", temp_s);
+            printf ("Percentual de reajuste correspondente ao salario atual: 5%%\n");
+            printf ("Tempo de servico: %i anos\n", temp_s);
             printf ("\n");
 
 
             if (temp_s > 5) {
                 calculo_percentual_2 = salario_a * 0.02;
-                printf ("Bônus adicional devido ao tempo de serviço: 2%%\n");
+                printf ("Bonus adicional devido ao tempo de servico: 2%%\n");
                 printf ("\n");
 
             }
@@ -165,21 +165,22 @@ int main () {
 
             if (salario_total < 1621.) {
                 printf ("***************ALERTA***************\n");
-                printf ("*Salário inferior ao salário mínimo!*\n");
+                printf ("*Salario inferior ao salario minimo!*\n");
                 printf ("***************ALERTA***************\n");
                 printf ("\n");
 
             }
-            printf ("Salário anterior: %.2f\n", salario_a);
+            printf ("Salario anterior: R$%.2f\n", salario_a);
             printf ("Percentual aplicado: 5%%\n");
-            printf ("Valor do aumento: %.2f\n", valor_aumento);
-            printf ("Novo salário: %.2f\n", salario_total);
+            printf ("Valor do aumento: R$%.2f\n", valor_aumento);
+            printf ("Novo salario: R$%.2f\n", salario_total);
             printf ("\n");
 
         }
     }
-    while (nome_f != "S" && nome_f != "s");{
+    while (nome_f != "S" && nome_f != "s");
         printf ("Programa encerrado");
-    }
+
     return 0;
 }
+
